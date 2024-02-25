832fea9fc8e4a3caf63bd6eeae56d7f72fe96053
JEASA <jean-luc.sauve@ist.com>
2024-02-25 20:15:29 +0200

    update config

--- backup-Negpod_11.sh
+++ backup-Negpod_11.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Configuration parameters
-remote_server="6a2d7e3b59f1.3a2627c1.alu-cod.online"
-remote_user="6a2d7e3b59f1"
+remote_server="64293e56bc62.3a2627c1.alu-cod.online"
+remote_user="64293e56bc62"
 remote_secret="328d3b338a4ced526c9a"
 remote_location="/summative/1023-2024j"
 local_location="negpod_11-q1"
